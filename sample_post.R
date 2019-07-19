6b0201a4c3364a3d676d2e8ae832798962e97f67
Guangchuang Yu <guangchuangyu@gmail.com>
2019-07-19 15:39:37 +0800

    automatically copy sample posts to clipboard

--- sample_post.R
+++ sample_post.R
@@ -5,7 +5,10 @@ y <- x[grep("weixin", x)]
 y <- y[grep("^\\+", y)]
 res <- sample(y, 8)
 res <- res[order(nchar(res, type="width"))]
-cat("----\n")
-cat('**往期精彩**\n')
-cat(paste(res, '\n', collapse=""))
 
+res <- paste("----\n**往期精彩**\n",
+          paste(res, '\n', collapse=""))
+
+clipr::write_clip(res, allow_non_interactive = TRUE)
+
+message("sample posts were copied to clipboard")
